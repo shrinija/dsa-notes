6d59e124092ab5e97d51908be7f0f6d6d7d0e97b
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-07 19:15:42 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -171,3 +171,104 @@ cout<<endl;
 }
 
 //......................................................................
+//-->using templates
+
+//stack using array
+//stack using array dynamic
+//-->github
+
+#include<iostream>
+using namespace std;
+
+template <typename T>
+
+class stack_using_array_dynamic_usingtemplate{
+    T *arr;
+    int nextindex;
+    int capacity;
+
+    public:
+    stack_using_array_dynamic_usingtemplate()
+    {
+        arr=new T[4];
+        nextindex=0;
+        capacity=4;
+    }
+    void push(T data)
+    {
+        if(nextindex==capacity)
+        {
+           T *newarr=new T[capacity*2];
+           for(int i=0;i<capacity;i++)
+           {
+            newarr[i]=arr[i];
+           }
+           capacity=capacity*2;
+           delete [] arr;                                   //delete karna ...?
+           arr=newarr;
+        }
+        arr[nextindex]=data;
+        nextindex++;
+    }
+    void pop()
+    {
+        if(nextindex==0)
+        {
+           cout<<"stack is empty";
+           return;
+        }
+        nextindex--;
+    }
+    T top()
+    {
+         if(nextindex==0)
+        {
+           cout<<"stack is empty";
+           return 0;
+        }
+        return arr[nextindex-1];
+    }
+    bool is_empty()
+    {
+        return nextindex==0;
+    }
+    int size()
+    {
+        return nextindex;
+    }
+
+};
+int main()
+{
+    stack_using_array_dynamic_usingtemplate <int>s;
+    s.push(1);
+    s.push(2);
+     s.push(1);
+    s.push(2);
+     s.push(1);
+    s.push(2);
+    s.pop();
+
+    cout<<s.top()<<endl;
+    s.pop();
+
+    cout<<s.top()<<endl;
+
+    cout<<s.size()<<endl;
+
+    cout<<s.is_empty()<<endl;
+
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+
+cout<<endl;
+    cout<<s.top()<<endl;
+
+    cout<<s.size()<<endl;
+
+    cout<<s.is_empty()<<endl;
+}
+//................................................
