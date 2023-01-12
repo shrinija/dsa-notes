3f758726ca6c8374508c3aea169183c2ad7691ab
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-12 10:12:37 +0530

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -30,3 +30,60 @@ int main()
 }
 
 //......................................................................................................................
+
+// take input and print tree   recursive
+
+#include <iostream>
+using namespace std;
+#include <vector>
+template <typename T>
+class treenode{
+public:
+  T data;
+  vector<treenode<T> *> children;
+  treenode(T data)
+  {
+    this->data = data;
+  }
+};
+
+treenode<int>* take_input_recursive()
+{
+  cout<<"enter root data : ";
+  int data;
+  cin>>data;
+treenode<int>*root=new treenode<int>(data);
+  cout<<"enter no of children of "<<data<<" : ";
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+    treenode<int>*child=take_input_recursive();
+    root->children.push_back(child);
+  }
+  return root;
+}
+
+void print_tree_recursive(treenode<int>*root)
+{
+  cout<<root->data<<" : ";
+  for(int i=0;i<root->children.size();i++)
+  {
+    cout<<root->children[i]->data<<" ";
+  }
+  cout<<endl;
+  for(int i=0;i<root->children.size();i++)
+  {
+    print_tree_recursive(root->children[i]);
+  }
+}
+
+
+
+int main()
+{
+treenode<int>*root=take_input_recursive();
+print_tree_recursive(root);
+}
+
+//.................................................................................................................
