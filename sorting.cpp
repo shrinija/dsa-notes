a9cbbe5d0543a63fdbb964ab1824b11a041d4906
shrinija <98155154+shrinija@users.noreply.github.com>
2023-08-03 16:09:16 +0530

    Update sorting.cpp
    sorting algo

--- sorting.cpp
+++ sorting.cpp
@@ -1,190 +1,145 @@
 
-// selection sort
+//...........................................................................................................
+//selection sort
+//best- O(n^2)
+//avg- O(n^2)
+//worst-O(n^2)
+//s.c- worst-O(1)
 
-#include <bits/stdc++.h>
+
+#include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n)
-{
-  int i, j, min_idx;
-
-  for (i = 0; i < n - 1; i++)
-  {
-    min_idx = i;
-    for (j = i + 1; j < n; j++)
-    {
-      if (arr[j] < arr[min_idx])
-        min_idx = j;
-    }
-
-    if (min_idx != i)
-      swap(arr[min_idx], arr[i]);
-  }
+void selection_sort(int arr[],int n){
+int min;
+int min_index;
+for(int i=0;i<n-1;i++){
+	min=arr[i];
+	min_index=i;
+  for(int j=i+1;j<n;j++){
+    if(arr[j]<min){
+			min=arr[j];
+			min_index=j;
+		}
+	}
+	swap(arr[i],arr[min_index]);
+}
 }
 
-void printArray(int arr[], int size)
-{
-  int i;
-  for (i = 0; i < size; i++)
-  {
-    cout << arr[i] << " ";
-    cout << endl;
-  }
+void print_array(int arr[],int n){
+   for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	 }
 }
 
 int main()
 {
-  int arr[] = {64, 25, 12, 22, 11};
-  int n = sizeof(arr) / sizeof(arr[0]);
+  int arr[]={4,5,2,3,1,7,6};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	cout<<sizeof(arr);
+	cout<<endl;
+	cout<<sizeof(arr[0]);
+	cout<<endl;
+	cout<<size;
+	cout<<endl;
 
-  selectionSort(arr, n);
-  cout << "Sorted array: \n";
-  printArray(arr, n);
-  return 0;
+   selection_sort(arr,size);
+	 cout<<"sorted array : ";
+	 print_array(arr,size);
 }
 
+//..................................................................................................
 
-//...................................................................................
+//Bubble Sort
+//best- O(n)
+//avg- O(n^2)
+//worst- O(n^2)
+//s.c - O(1)
 
-// Bubble sort
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-void bubbleSort(int arr[], int n)
+void Bubble_sort(int arr[], int n)
 {
-  int i, j;
-  bool swapped;
-  for (i = 0; i < n - 1; i++)
-  {
-    swapped = false;
-    for (j = 0; j < n - i - 1; j++)
-    {
-      if (arr[j] > arr[j + 1])
-      {
-        swap(arr[j], arr[j + 1]);
-        swapped = true;
-      }
-    }
-    if (swapped == false)
-      break;
-  }
+	bool swapped;
+	for(int i=0;i<n-1;i++){
+		swapped=false;
+		for(int j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+          swap(arr[j],arr[j+1]);
+					swapped=true;
+			}
+		}
+    if(swapped==false)return;
+	}
 }
 
-void printArray(int arr[], int size)
+void print_array(int arr[], int n)
 {
-  int i;
-  for (i = 0; i < size; i++)
-    cout << " " << arr[i];
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
 }
 
 int main()
 {
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
-  int N = sizeof(arr) / sizeof(arr[0]);
-  bubbleSort(arr, N);
-  cout << "Sorted array: \n";
-  printArray(arr, N);
-  return 0;
+	int arr[] = {4, 5, 2, 3, 1, 7, 6};
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	Bubble_sort(arr, size);
+	cout << "sorted array : ";
+	print_array(arr, size);
 }
-//...................................................................................
 
-// insertion sort
+//.................................................................................
+//insertion sort
+//best- O(n)
+//avg- O(n^2)
+//worst- O(n^2)
+//s.c - O(1)
 
-#include <bits/stdc++.h>
+
+#include <iostream>
 using namespace std;
-
-void insertionSort(int arr[], int n)
+void Insertion_sort(int arr[], int n)
 {
-  int i, key, j;
-  for (i = 1; i < n; i++)
-  {
-    key = arr[i];
-    j = i - 1;
-
-    while (j >= 0 && arr[j] > key)
-    {
-      arr[j + 1] = arr[j];
-      j = j - 1;
-    }
-    arr[j + 1] = key;
-  }
+	for(int i=1;i<n;i++){
+		int temp=arr[i];
+		int j=i-1;
+    for(;j>=0;j--){
+			if(arr[j]>temp){
+				arr[j+1]=arr[j];
+			}
+			else{
+				break;
+			}
+		}
+		arr[j+1]=temp;
+	}
 }
 
-void printArray(int arr[], int n)
+void print_array(int arr[], int n)
 {
-  int i;
-  for (i = 0; i < n; i++)
-    cout << arr[i] << " ";
-  cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
 }
 
 int main()
 {
-  int arr[] = {12, 11, 13, 5, 6};
-  int N = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = {4, 5, 2, 3, 1, 7, 6};
+	int size = sizeof(arr) / sizeof(arr[0]);
 
-  insertionSort(arr, N);
-  printArray(arr, N);
-
-  return 0;
+	Insertion_sort(arr, size);
+	cout << "sorted array : ";
+	print_array(arr, size);
 }
+//.......................................................................................
 
-//...................................................................
+//merge Sort
+//best- O(nlog(n))
+//avg- O(nlog(n))
+//worst- O(nlog(n))
+//s.c - O(n)
 
-//Merge sort
-
-void merge(vector<int> &arr, vector<int> &temp, int l, int m, int r)
-{
-
-  int i = l, j = m + 1, k = l;
-
-  while (i <= m && j <= r)
-  {
-    if (arr[i] <= arr[j])
-    {
-      temp[k++] = arr[i++];
-    }
-    else
-    {
-      temp[k++] = arr[j++];
-    }
-  }
-
-  while (i <= m)
-  {
-    temp[k++] = arr[i++];
-  }
-
-  while (j <= r)
-  {
-    temp[k++] = arr[j++];
-  }
-
-  for (int a = l; a <= r; a++)
-  {
-    arr[a] = temp[a];
-  }
-}
-
-void mergeSort(vector<int> &arr, vector<int> &temp, int l, int r)
-{
-  if (l < r)
-  {
-    int m = (l + r) / 2;
-    mergeSort(arr, temp, l, m);
-    mergeSort(arr, temp, m + 1, r);
-    merge(arr, temp, l, m, r);
-  }
-}
-
-
-
-
-// Merge Sort Complexity
-// Time Complexity	 
-// Best	O(n*log n)
-// Worst	O(n*log n)
-// Average	O(n*log n)
-// Space Complexity	O(n)
-// Stability	Yes
